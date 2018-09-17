3c07aac0b4f5ae67bdc84222343069461e7dc615
aryaannurseno1 <aaryasenoo@gmail.com>
2018-09-17 17:53:52 +0700

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import './App.css';
+import Profile from './components/profile.js';
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <p>Halo</p>
+        <Profile/>
+      </div>
+      
+    );
+  }
+}
+
+export default App;
---
+++ src/components/profile.js
@@ -0,0 +1,23 @@
+import React, { Component } from 'react';
+
+ class Profile extends Component{
+
+    render(){
+        return(
+            
+            <div>
+                <ul>
+                    <li> Nama : seno</li>
+                <li>BBB</li>
+                <li>CCC</li>
+
+
+                </ul>
+            </div>
+
+
+        );
+
+    }
+}
+export default Profile;
